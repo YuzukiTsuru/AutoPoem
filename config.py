493813627bc03b745224d6a030085148bb748345
740291272 <740291272@qq.com>
2019-07-04 17:04:07 +0800

    233

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config(object):
     poetry_file = 'dataset/poetry.txt'
-    weight_file = 'poetry_model.h5'
+    weight_file = '/home/cl/cl/AutoPoem/poetry_model.h5'
     # 根据前六个字预测第七个字
     max_len = 6
     batch_size = 128
