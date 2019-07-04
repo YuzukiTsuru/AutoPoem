aa3dc06ed6f6e63948ddd2b899ebfb4603180662
740291272 <740291272@qq.com>
2019-07-04 16:54:27 +0800

    first add

--- Random.py
+++ Random.py
@@ -6,6 +6,5 @@ if __name__ == '__main__':
     model = PoetryModel(Config)
 
     for temp in [0.5, 1, 1.5]:
-        # 随机抽取第一句话进行预测
         sen = model.predict_random(temperature=temp)
         print(sen)
--- Tibetan_poetry.py
+++ Tibetan_poetry.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     model = PoetryModel(Config)
     for i in range(3):
         # 藏头诗
-        sen = model.predict_hide('争云日夏')
+        sen = model.predict_hide('我不要你')
         print(sen)
 
